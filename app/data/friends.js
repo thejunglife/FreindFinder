4e25ae0a1735e3714583df89fdde7dc3fee4bcf1
Paul <thejunglife@gmail.com>
2019-11-19 03:12:06 -0800

    finished algorithim for best match

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,22 @@ const friends = [
     3,
     3
   ]
+},
+{
+  "name": "John",
+  "photo": "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/12096498_10201351719990825_924240634026425844_n.jpg?_nc_cat=103&_nc_oc=AQkalXRWrUNPk1pc2xpXfDU023Upa8RGhkidA8id9zyV5fTx50wkdWdSFrZA3Ekrqyo&_nc_ht=scontent-den4-1.xx&oh=e24b3cb7c72194e033449ee2c65fc5d5&oe=5E3FB7CB",
+  "scores": [
+2,
+2,
+2,
+2,
+2,
+2,
+2,
+2,
+2,
+2
+  ]
 }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,45 @@ const friend = require('../data/friends.js')
 
 
 module.exports = app => {
+
     app.get('/api/friends', (req, res) => {
       res.send(friend)
     })
 
     app.post('/api/friends', (req, res) => {
-      friend.push(req.body)
-      res.sendStatus(200)
-    })
-}
+      
+      // get user info
+      let user = req.body
+      
+      // will be the index of the best matched friend
+      let bestMatch = 0
+      // default difference for comparing 
+      let defaultDiff = 51
+
+      for (let i = 0; i < friend.length; i++) {
+
+        let totalDiff = 0
+
+          for (let j = 0; j < friend[i].scores.length; j++) {
+           
+            let difference = Math.abs(friend[i].scores[j] - user.scores[j])
+            totalDiff += difference
+            
+          }
+
+          if (totalDiff < defaultDiff) {
+            defaultDiff = totalDiff
+            bestMatch = i
+
+          }
+      
+     
+      
+    }
+   
+    friend.push(user)
+
+    res.json(friend[bestMatch])
+  })
+
+  }
